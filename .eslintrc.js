558a13eaa148d3d502f1e8458ee8e1487e67ec2c
Martin <mr.mmaurer+work@gmail.com>
2021-05-01 23:25:25 +0100

    init project and added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        "browser": true,
+        "es6": true
+    },
+    extends: [
+        "airbnb-typescript/base",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    globals: {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "project": [".tsconfig"],
+    },
+    plugins: [
+        'svelte3'
+    ],
+    overrides: [{
+        files: ['*.svelte'],
+        processor: 'svelte3/svelte3'
+    }],
+    settings: {
+        'svelte3/typescript': true,
+    },
+    rules: {}
+};
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import HelloWorld from "./components/HelloWorld.svelte";
+import logo from "./assets/logo.png";
+</script>
+
+<div id="app">
+  <img alt="Svelte logo" src={logo}>
+  <HelloWorld message="Svelte"/>
+</div>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
