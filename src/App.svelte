692644365220b3d05956666fcc0e272e78f5113a
Martin <mr.mmaurer+work@gmail.com>
2021-05-07 23:33:34 +0100

    Added proper sanitise imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,29 @@
 <script lang="ts">
-  import "sanitize.css/sanitize.css";
+  import "sanitize.css/evergreen.css";
+  import "sanitize.css/typography.css";
+
   import Row from "./components/Row.svelte";
   import RowCopy from "./components/RowCopy.svelte";
   import RowClamp from "./components/RowClamp.svelte";
 
-  let title: string = 'Original'
-  let titleCopy: string = 'Copy'
-  let titleClamp: string = 'Clamp'
 </script>
 
 <template>
   <div id="app">
-    <Row title={title} />
+    <Row title="Original" />
     <hr />
-    <RowCopy title={titleCopy} />
+    <RowCopy title="Copy" />
     <hr />
-    <RowClamp title={titleClamp} />
+    <RowClamp title="Clamp" />
   </div>
 </template>
 
 <style lang="scss">
   #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin-top: 50px;
   }
 </style>
